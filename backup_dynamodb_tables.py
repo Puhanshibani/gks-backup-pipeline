34db68b132fb0011df5f736579ba39cf357961e8
Suraj Dubal <Suraj.ravikantdubal@Vodafone.com>
2021-12-28 22:01:05 +0530

    files updated

--- backup_dynamodb_tables.py
+++ backup_dynamodb_tables.py
@@ -7,10 +7,8 @@ REGION = os.getenv("REGION")
 dynamodb = boto3.resource("dynamodb", region_name=REGION)
 tables = []
 
-# print("Table Name", os.environ["BACKUP_TABLE_NAME"])
-# print(type(os.environ["BACKUP_TABLE_NAME"]))
 
-for i in range(5):  # BACKUP_TABLE_NAME_1
+for i in range(4):  # BACKUP_TABLE_NAME_1
     TABLE_NAME = "BACKUP_TABLE_NAME_" + str(i)
     if os.environ[TABLE_NAME] != "NULL":
         print("Backing up TABLE_NAME=", os.environ[TABLE_NAME])
